9c628e88e22c7644cadd36a722470fccf4fbd1c0
ricardo <ramn21@gmail.com>
2021-04-22 00:59:18 +0200

    Generate rails app, scaffold books model

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(
+  {
+    title: 'The Girl Beneath the Sea',
+    category: 'Action',
+  }
+)
+Book.create(
+  {
+    title: 'It',
+    category: 'Horror',
+  }
+)
+Book.create(
+  {
+    title: 'React: Up and Running',
+    category: 'Learning',
+  }
+)
+Book.create(
+  {
+    title: 'Ruby on Rails: Up and Running',
+    category: 'Learning',
+  }
+)
